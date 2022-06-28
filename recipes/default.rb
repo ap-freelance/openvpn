fc82d49a0124a9766c80148a869cadaaa786023b
ap-freelance <101653563+ap-freelance@users.noreply.github.com>
2022-06-28 10:38:54 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -153,7 +153,7 @@ if true
   end
    
   server_databags = Chef::DataBag.list(true)["openvpn-#{server_name}"]
-  clients = server_databags.keys.reject { |x| x =~ /openvpn/ }
+  clients = server_databags.reject { |x| x =~ /openvpn/ }
   
   template "/etc/openvpn/#{server_name}/otp_secrets" do
     source "otp-secrets.erb"
